77d5c6ab1305999f4500858ac1696a6d39f12bca
nagolove <dekar.own@gmail.com>
2024-04-27 17:52:53 +0300

    1 task input

--- main1.c
+++ main1.c
@@ -6,20 +6,36 @@
 #ifdef ONLINE_JUDGE 
 #endif
 
-void task(char *line) {
-    int a, b, c;
-    fprintf(stderr, "line '%s'\n", line);
-    sscanf(line, "%d %d %d", &a, &b, &c);
-    if (c > b && b > a)
-        printf("STAIR\n");
-    else if (b > a && b > c) 
-        printf("PEAK\n");
-    else
-        printf("NONE\n");
+static FILE *E;
+
+
+void task(char *line1, char *line2) {
+    fprintf(E, "'%s', '%s'\n", line1, line2);
+    int n = 0, k = 0;
+    sscanf(line1, "%d %d\n", &n, &k);
+    int nums[n];
+    memset(nums, 0, sizeof(nums));
+
+    char *token = strtok(line2, " ");
+    int i = 0;
+    while(token) {
+        //puts(token);
+
+        sscanf(token, "%d", &nums[i++]);
+
+        token = strtok(NULL, " ");
+    }
+
+    fprintf(stderr, "n %d, k %d\n", n, k);
+    for (int i = 0; i < n; i++) {
+        fprintf(stderr, "%d ", nums[i]);
+    }
+    fprintf(stderr, "\n");
 }
 
 
 int main(int argc, char **argv) {
+    E = stderr;
 
 #ifndef ONLINE_JUDGE 
     freopen("input1.txt", "rt", stdin); 
@@ -27,18 +43,22 @@ int main(int argc, char **argv) {
 #endif
 
     size_t max_line_len = 128;
-    char line[max_line_len];
+    char line1[max_line_len], line2[max_line_len];
     int num = 0;
     scanf("%d", &num);
+    fgets(line1, max_line_len, stdin);
 //#ifndef ONLINE_JUDGE
     fprintf(stderr, "num %d\n", num);
 //#endif
-    fgets(line, max_line_len, stdin);
-    while (fgets(line, max_line_len, stdin)) {
-        size_t len = strlen(line);
-        line[len - 1] = 0;
+    for (int i = 0; i < num; ++i) {
+        fgets(line1, max_line_len, stdin);
+        fgets(line2, max_line_len, stdin);
+        size_t len = strlen(line1);
+        line1[len - 1] = 0;
+        len = strlen(line2);
+        line2[len - 1] = 0;
         //printf("%s\n", line);
-        task(line);
+        task(line1, line2);
     }
 
     return EXIT_SUCCESS;
