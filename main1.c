2dfba48d1ac75aada2155a62d8ed7f8cf1cce4d8
nagolove <dekar.own@gmail.com>
2024-05-20 18:14:55 +0300

    1

--- main1.c
+++ main1.c
@@ -17,10 +17,50 @@ int _cmp_map(const void *a, const void *b) {
     return _b->rep - _a->rep;
 }
 
-void task(int a, int b) {
-    printf("%d %d\n", a < b ? a : b, a > b ? a : b);
-}
+static int test_num = 0;
 
+void task(int a1, int b2) {
+    test_num++;
+
+    int cells = 0;
+    fprintf(E, "%d task: %d %d\n", test_num, a1, b2);
+
+    /*
+    На экране - два больших и 7 малых значков
+    или 15 малых значков
+     */
+    int ans = 0;
+
+    int k = b2 / 2 + (b2 % 2);
+
+    if (a1 == 0 && b2 == 0) {
+        printf("0\n");
+        return;
+    }
+
+    if (b2 == 1 || b2 == 2 || b2 == 0)
+        k = 1;
+
+    fprintf(E, "k %d\n", k);
+    cells = k * 15 - b2 * 4; // количество свободных еденичных ячеек
+
+    fprintf(E, "cells %d\n", cells);
+
+    if (a1 > cells) {
+        fprintf(E, "needed %d\n", a1 - cells);
+        int d1 = (a1 - cells) / 15;
+        if (d1 == 0)
+            d1 = 1;
+        int d2 = 15 % (a1 - cells);
+        fprintf(E, "d1 %d, d2 %d\n", d1, d2);
+        ans += d1 + d2;
+    }
+
+    ans += k;
+
+    printf("%d\n", ans);
+    fprintf(E, "ans %d\n", ans);
+}
 
 int main(int argc, char **argv) {
     E = stderr;
@@ -39,11 +79,8 @@ int main(int argc, char **argv) {
     fprintf(stderr, "num %d\n", num);
 //#endif
     for (int i = 0; i < num; ++i) {
-        fgets(line1, max_line_len, stdin);
-        size_t len = strlen(line1);
-        fprintf(E, "%s\n", line1);
         int a = 0, b = 0;
-        sscanf(line1, "%d %d", &a, &b);
+        scanf("%d %d", &a, &b);
         task(a, b);
     }
 
